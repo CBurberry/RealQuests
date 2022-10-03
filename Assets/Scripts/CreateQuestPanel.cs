e5f2396f276d1eb79834c3595f0dc687dd449bff
Christian <christianburberry@gmail.com>
2022-10-03 08:31:05 +0100

    Added panel script to process text inputs and call AppManager.CreateNewQuestItem

---
+++ Assets/Scripts/CreateQuestPanel.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateQuestPanel : MonoBehaviour
+{
+    [SerializeField]
+    private InputField titleInput;
+
+    [SerializeField]
+    private Dropdown rewardDropdown;
+
+    [SerializeField]
+    private InputField rewardCountInput;
+
+    //UI Elements to show
+    [SerializeField]
+    private GameObject backButton;
+
+    [SerializeField]
+    private GameObject addNewQuestButton;
+
+    public void CreateQuest()
+    {
+        if (string.IsNullOrWhiteSpace(titleInput.text)) 
+        {
+            return;
+        }
+
+        int count;
+        if (!int.TryParse(rewardCountInput.text, out count) || count <= 0) 
+        {
+            return;
+        }
+
+        backButton.SetActive(true);
+        addNewQuestButton.SetActive(true);
+        gameObject.SetActive(false);
+        AppManager.Instance.AddNewQuestItem(titleInput.text, RewardType.LuxuryToken, count);
+    }
+}
